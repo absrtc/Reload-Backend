00c317e3f00c38a07ad41b70e7ac9b714a9b3647
absrtc <pixelstrap9@gmail.com>
2026-02-12 15:26:45 -0700

    add paks to config

--- routes/paks.js
+++ routes/paks.js
@@ -4,28 +4,30 @@ const path = require("path");
 
 const app = express.Router();
 
-const paksdir = path.join("C:\\Users\\User\\Documents\\testPath\\Fortnite 8.51\\FortniteGame\\Content\\Paks");
+const config = JSON.parse(fs.readFileSync("./Config/config.json").toString());
 
 app.get("/api/launcher/paks", (req, res) => {
-    fs.readdir(paksdir , (err, f) => {
-        if (err) {
-            return res.status(500).json({ error: "Failed to read paks, build may be invalid!" });
-        }
+  fs.readdir(config.launcher.paksDir, (err, f) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: "Failed to read paks, build may be invalid!" });
+    }
 
-        const files = f
-            .filter(file => file.endsWith(".pak"))
-            .map(file => {
-                const filePath = path.join(PAKS_DIR, file);
-                const stats = fs.statSync(filePath);
+    const files = f
+      .filter((file) => file.endsWith(".pak"))
+      .map((file) => {
+        const filePath = path.join(config.launcher.paksDir);
+        const stats = fs.statSync(filePath);
 
-                return {
-                    name: file,
-                    size: stats.size
-                };
-            });
+        return {
+          name: file,
+          size: stats.size,
+        };
+      });
 
-        res.json(files);
-    });
+    res.json(files);
+  });
 });
 
 module.exports = app;
